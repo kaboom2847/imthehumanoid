3b79d79980535b2e59918deaf2bd5e7a05f00d9c
kaboom2847 <72105597+kaboom2847@users.noreply.github.com>
2021-08-23 01:53:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+function reddit() {location.href="https://reddit.com/u/nameofanuser/"}
+function instagram() {location.href="https://instagram.com/imthehumanoid/"}
+function twitter() {location.href="https://twitter.com/imthehumanoid/"}
